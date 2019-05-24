fc89d80a78c3def26efdc1b997bd046396203ee7
Frank Terbeck <ft@bewatermyfriend.org>
2019-05-24 20:09:46 +0200

    Only announce the files that exists when they are being read
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- makemehappy/yamlstack.py
+++ makemehappy/yamlstack.py
@@ -8,15 +8,17 @@ class YamlStack:
         self.desc = desc
         self.files = lst
         self.data = False
-        for item in lst:
-            self.log.info("{}: {}".format(desc, item))
 
     def push(self, item):
         self.log.info("{}: {}".format(self.desc, item))
         self.files = self.files + [item]
 
+    def fileload(self, fn):
+        self.log.info("Loading {}: {}".format(self.desc, fn))
+        return mmh.load(fn)
+
     def load(self):
-        self.data = list((mmh.load(x) for x in self.files
+        self.data = list((self.fileload(x) for x in self.files
                           if os.path.isfile(x)))
 
     def lookup(self, needle):
