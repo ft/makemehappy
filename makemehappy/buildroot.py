1b235f6746a8d7433084768fecb64d971fae5edd
Frank Terbeck <ft@bewatermyfriend.org>
2019-05-25 01:33:59 +0200

    Add --preserve to preserve temporary build dirs
    In default operation, they are deleted.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- makemehappy/buildroot.py
+++ makemehappy/buildroot.py
@@ -1,32 +1,54 @@
+import datetime
+import hashlib
 import os
 import shutil
-import tempfile
+import time
+import yaml
+
+def timeString():
+    now = datetime.datetime.now()
+    return now.strftime("%Y-%m-%d_%H:%M:%S.%f")
+
+def tempString(string):
+    s = string + timeString()
+    return hashlib.sha1(s.encode('utf-8')).hexdigest()
+
+def tempDirName(mod):
+    prefix = 'mmh-'
+    if ('name' in mod):
+        prefix = prefix + mod['name'] + '-'
+    suffix = '-root'
+    return prefix + tempString(yaml.dump(mod)) + suffix
+
+def mkTempDir(mod):
+    root = str(os.getenv('TMPDIR', os.path.join('/', 'tmp')))
+    while True:
+        d = os.path.join(root, tempDirName(mod))
+        try:
+            os.mkdir(d)
+            return d
+        except FileExistsError:
+            time.sleep(0.1)
 
 class BuildRoot:
-    def __init__(self, log, name = None):
+    def __init__(self, log, mod, name = None):
         self.initdirs = [ 'build', 'deps', 'tools' ]
         self.log = log
         self.calldir = os.path.realpath(os.getcwd())
         if (name == None):
-            self.root = tempfile.TemporaryDirectory(prefix = 'mmh-',
-                                                    suffix = '-root')
+            self.root = mkTempDir(mod.data)
         else:
             self.root = name
-            os.mkdir(name, 0o755)
-        self.log.info("Setting up build-directory: {}".format(self.name()))
+            os.mkdir(self.root, 0o755)
+
+        self.log.info("Setting up build-directory: {}".format(self.root))
 
     def name(self):
-        if (isinstance(self.root, tempfile.TemporaryDirectory)):
-            return self.root.name
-        else:
-            return self.root
+        return self.root
 
     def cleanup(self):
         self.log.info("Cleaning up build-directory: {}".format(self.name()))
-        if (isinstance(self.root, tempfile.TemporaryDirectory)):
-            return self.root.cleanup()
-        else:
-            shutil.rmtree(self.root)
+        shutil.rmtree(self.root)
 
     def cd(self):
         self.log.info("Changing into build-directory: {}".format(self.name()))
