a16a08b28012e3a86e6571b71a0f66b739b7a8b2
Frank Terbeck <ft@bewatermyfriend.org>
2019-05-25 02:16:38 +0200

    Run test-suite after build as well
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- makemehappy/build.py
+++ makemehappy/build.py
@@ -2,6 +2,10 @@ import os
 import subprocess
 
 def allofthem():
+    olddir = os.getcwd()
     subprocess.run(['cat', 'CMakeLists.txt'])
     subprocess.run(['cmake', '-GUnix Makefiles', '-S', '.', '-B', 'build'])
     subprocess.run(['cmake', '--build', 'build'])
+    os.chdir('build')
+    subprocess.run(['ctest', '-VV'])
+    os.chdir(olddir)
--- makemehappy/toplevel.py
+++ makemehappy/toplevel.py
@@ -3,6 +3,8 @@ def generateToplevel(fname):
         print("cmake_minimum_required(VERSION 3.1.0)", file = fh)
         print("project(ufw C CXX ASM)", file = fh)
         print('list(APPEND CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/code-under-test/cmake/modules")', file = fh)
+        print("include(CTest)", file = fh)
         print("include(Libtap)", file = fh)
+        print("enable_testing()", file = fh)
         print("add_libtap(deps/libtap)", file = fh)
         print("add_subdirectory(code-under-test)", file = fh)
