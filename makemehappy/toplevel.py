07eae545450ad84f536143c09c6c9a8c86f24504
Frank Terbeck <ft@bewatermyfriend.org>
2019-05-26 19:00:11 +0200

    Split top-level generation into suitable sub procedures
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- makemehappy/toplevel.py
+++ makemehappy/toplevel.py
@@ -1,10 +1,30 @@
-def generateToplevel(fname):
+defaultCMakeVersion = "3.1.0"
+defaultProjectName = "MakeMeHappy"
+defaultLanguages = "C CXX ASM"
+
+def generateHeader(fh):
+    for s in ["cmake_minimum_required(VERSION 3.1.0)".format(defaultCMakeVersion),
+              "project({} {})".format(defaultProjectName, defaultLanguages)]:
+        print(s, file = fh)
+
+def generateCMakeModulePath(fh):
+    print('list(APPEND CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/code-under-test/cmake/modules")', file = fh)
+
+def generateTestHeader(fh):
+    print("include(CTest)", file = fh)
+    print("enable_testing()", file = fh)
+
+def generateDependencies(fh):
+    print("include(Libtap)", file = fh)
+    print("add_libtap(deps/libtap)", file = fh)
+
+def generateFooter(fh):
+    print("add_subdirectory(code-under-test)", file = fh)
+
+def generateToplevel(log, cfg, src, fname):
     with open(fname, 'w') as fh:
-        print("cmake_minimum_required(VERSION 3.1.0)", file = fh)
-        print("project(ufw C CXX ASM)", file = fh)
-        print('list(APPEND CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/code-under-test/cmake/modules")', file = fh)
-        print("include(CTest)", file = fh)
-        print("include(Libtap)", file = fh)
-        print("enable_testing()", file = fh)
-        print("add_libtap(deps/libtap)", file = fh)
-        print("add_subdirectory(code-under-test)", file = fh)
+        generateHeader(fh)
+        generateCMakeModulePath(fh)
+        generateTestHeader(fh)
+        generateDependencies(fh)
+        generateFooter(fh)
