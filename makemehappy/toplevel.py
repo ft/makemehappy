409c6fd1a989b9c60d614f89855d27405965e2c7
Frank Terbeck <ft@bewatermyfriend.org>
2019-07-01 14:25:37 +0200

    Support boolean include statements in cmake-extensions
    This is useful for modules that don't build any code, to avoid adding any
    build-system inclusions in toplevel cmake files.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- makemehappy/toplevel.py
+++ makemehappy/toplevel.py
@@ -17,8 +17,10 @@ def generateTestHeader(fh):
 
 def insertInclude(fh, name, tp):
     if (name in tp):
-        print("include({})".format(tp[name]['module']), file = fh)
-        print("{}(deps/{})".format(tp[name]['include'], name), file = fh)
+        inc = tp[name]['include']
+        if (isinstance(inc, str)):
+            print("include({})".format(tp[name]['module']), file = fh)
+            print("{}(deps/{})".format(inc, name), file = fh)
     else:
         print("add_subdirectory(deps/{})".format(name), file = fh)
 
