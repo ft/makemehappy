34ac6895fbe4e5ebfedbfc910f36c62466c51b59
Frank Terbeck <ft@bewatermyfriend.org>
2025-09-27 12:53:29 +0200

    doc: Determine package version at compile time
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- doc-ng/conf.py
+++ doc-ng/conf.py
@@ -1,16 +1,32 @@
 # Configuration file for the Sphinx documentation builder.
 
+# Python Environment Extensions
+
+import sys
+from pathlib import Path
+
+MMH_BASE = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(MMH_BASE))
+
+import makemehappy.utilities as mmh  # noqa: E402
+
+# Basic Information
+
 project = 'MakeMeHappy'
 author = 'Frank Terbeck'
-release = '0.34'
+release = mmh.stdoutProcess(str(MMH_BASE / 'version'))[0]
 
 extensions = []
 # templates_path = ['_templates']
 exclude_patterns = ['build', '.env']
 
+# HTML Output Setup
+
 html_theme = 'pydata_sphinx_theme'
 # html_static_path = ['_static']
 html_show_sourcelink = False
+
+# LaTeX/PDF Output Setup
 
 latex_theme = 'manual'
 latex_elements = {
